061a00a26aabf3e5db96a1ea75b5fcedc328beaf
Tom Close <tclose@oist.jp>
2014-04-08 11:48:56 +0900

    changed the import of scripts

--- scripts/sge_submit.py
+++ scripts/sge_submit.py
@@ -9,21 +9,20 @@ from neurotune.tuner.mpi import SGESubmitter
 script_name = sys.argv[1]
 # Create submitter object
 submitter = SGESubmitter()
-# Try to import argument parser from script in order to add those arguments to the SGE-related
-# arguments
-try:
-    exec("from {} import parser as script_parser".format(script_name))
-    parser, script_args = submitter.add_sge_arguments(script_parser)  # @UndefinedVariable: script_parser
-except ImportError:
-    print "Warning: failed to import 'parser' from '{}'".format(script_name)
-    script_parser = None
-    parser, script_args = submitter.add_sge_arguments(argparse.ArgumentParser())
+# Import the script as a module
+script = None # Actually set by the following 'exec' statement but initialised here to squash PyLint
+if os.path.dirname(script_name):
+    sys.path.append(os.path.dirname(script_name))
+exec("import {} as script".format(os.path.splitext(os.path.basename(script_name))[0]))
+# Place empty versions of parser and src_dir_init if they are not provided by script
+if not hasattr(script, 'parser'):  
+    script.parser = argparse.ArgumentParser() 
+if not hasattr(script, 'src_dir_init'): 
+    def dummy_func(src_dir, args):
+        pass
+    script.src_dir_init = dummy_func
+parser, script_args = submitter.add_sge_arguments(script.parser)  # @UndefinedVariable: script
 # Try to import 'src_dir_init' method from script otherwise fail gracefully
-try:
-    exec("from {} import src_dir_init".format(script_name))
-except ImportError:
-    print "Warning: failed to import 'src_dir_init' from '{}'".format(script_name)
-    src_dir_init = None
 # Parse arguments that were supplied to script
 args = parser.parse_args(sys.argv[2:])
 # Create work dir on 
@@ -33,7 +32,6 @@ cmdline = submitter.create_cmdline(script_name, script_args, work_dir, args)
 # Initialise work directory
 submitter.work_dir_init(work_dir)
 # Copy and 
-if src_dir_init is not None:
-    src_dir_init(os.path.join(work_dir, 'src'), args)
+script.src_dir_init(os.path.join(work_dir, 'src'), args)
 # Submit script to scheduler
 submitter.submit(script_name, cmdline, work_dir, output_dir, args, dry_run=True)
