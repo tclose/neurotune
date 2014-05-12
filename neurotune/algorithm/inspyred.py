b1258332d2f9a15f05e3f6bbe31179a1d5fc8735
Tom Close <tclose@oist.jp>
2014-05-12 20:37:12 +0900

    started cleaning up and trying to understand inspyred package

--- neurotune/algorithm/inspyred.py
+++ neurotune/algorithm/inspyred.py
@@ -105,18 +105,125 @@ class InspyredAlgorithm(Algorithm):
             self.indiv_file.close()
 
 
-class EDAAlgorithm(InspyredAlgorithm):
+class MultiObjectiveInspyredAlgorithm(InspyredAlgorithm):
 
-    _InspyredClass = ec.EDA
-    
-
-class NSGA2Algorithm(InspyredAlgorithm):
-
-    _InspyredClass = ec.emo.NSGA2
+    __metaclass__ = ABCMeta  # Declare this class abstract to avoid accidental construction
 
     def optimize(self, evaluator, **kwargs):
         # Wrap the list returned from the multi-objective objective in the required class
         # after it is evaluated (this saves having to import the inspyred module into 
         # objective.combined, an allows it to be more general)
-        super(EDAAlgorithm, self).optimize(lambda c: ec.emo.Pareto(evaluator(c)), **kwargs)
+        super(MultiObjectiveInspyredAlgorithm, self).optimize(lambda c: ec.emo.Pareto(evaluator(c)),
+                                                              **kwargs)
+            
         
+class GAAlgorithm(InspyredAlgorithm):
+    """
+    num_selected – the number of individuals to be selected (default len(population))
+    crossover_rate – the rate at which crossover is performed (default 1.0)
+    num_crossover_points – the n crossover points used (default 1)
+    mutation_rate – the rate at which mutation is performed (default 0.1)
+    num_elites – number of elites to consider (default 0)
+    """
+    _InspyredClass = ec.GA
+    
+
+class EDAAlgorithm(InspyredAlgorithm):
+    """
+    Evolutionary computation representing a canonical estimation of distribution algorithm.
+    This class represents an estimation of distribution algorithm which uses, by default, 
+    truncation selection, an internal estimation of distribution variation, and generational 
+    replacement. It is expected that each candidate solution is a Sequence of real values.
+     
+    The variation used here creates a statistical model based on the set of candidates. The 
+    offspring are then generated from this model. This function also makes use of the bounder 
+    function as specified in the EC’s evolve method.
+     
+    Optional keyword arguments in evolve args parameter:
+     
+    num_selected – the number of individuals to be selected (default len(population)/2)
+    num_offspring – the number of offspring to create (default len(population))
+    num_elites – number of elites to consider (default 0)
+    """
+    _InspyredClass = ec.EDA
+    
+
+class ESAlgorithm(InspyredAlgorithm):
+    """
+    tau – a proportionality constant (default None)
+    tau_prime – a proportionality constant (default None)
+    epsilon – the minimum allowed strategy parameter (default 0.00001)
+    If tau is None, it will be set to 1 / sqrt(2 * sqrt(n)), where n is the length of a candidate. 
+    If tau_prime is None, it will be set to 1 / sqrt(2 * n). The strategy parameters are updated as 
+    follows:
+     
+    σ′i=σi+eτ⋅N(0,1)+τ′⋅N(0,1)
+    σ′i=max(σ′i,ϵ)
+    """
+    _InspyredClass = ec.ES
+    
+    
+class DEAAlgorithm(InspyredAlgorithm):
+    """
+    Evolutionary computation representing a differential evolutionary algorithm.
+     
+    This class represents a differential evolutionary algorithm which uses, by default, tournament
+    selection, heuristic crossover, Gaussian mutation, and steady-state replacement. It is expected
+    that each candidate solution is a Sequence of real values.
+     
+    Optional keyword arguments in evolve args parameter:
+     
+    num_selected – the number of individuals to be selected (default 2)
+    tournament_size – the tournament size (default 2)
+    crossover_rate – the rate at which crossover is performed (default 1.0)
+    mutation_rate – the rate at which mutation is performed (default 0.1)
+    gaussian_mean – the mean used in the Gaussian function (default 0)
+    gaussian_stdev – the standard deviation used in the Gaussian function (default 1)
+    """
+    _InspyredClass = ec.DEA
+    
+    
+class SAAlgorithm(InspyredAlgorithm):
+    """
+    Evolutionary computation representing simulated annealing.
+     
+    This class represents a simulated annealing algorithm. It accomplishes this by using default
+    selection (i.e., all individuals are parents), Gaussian mutation, and simulated annealing
+    replacement. It is expected that each candidate solution is a Sequence of real values. Consult
+    the documentation for the simulated_annealing_replacement for more details on the keyword
+    arguments listed below.
+     
+    Note The pop_size parameter to evolve will always be set to 1, even if a different value is
+    passed. Optional keyword arguments in evolve args parameter:
+     
+    temperature – the initial temperature
+    cooling_rate – a real-valued coefficient in the range (0, 1) by which the temperature should be reduced
+    mutation_rate – the rate at which mutation is performed (default 0.1)
+    gaussian_mean – the mean used in the Gaussian function (default 0)
+    gaussian_stdev – the standard deviation used in the Gaussian function (default 1)        
+    """
+    _InspyredClass = ec.SA
+    
+
+class NSGA2Algorithm(MultiObjectiveInspyredAlgorithm):
+    """
+    Evolutionary computation representing the nondominated sorting genetic algorithm.
+
+    This class represents the nondominated sorting genetic algorithm (NSGA-II) of Kalyanmoy Deb et
+    al. It uses nondominated sorting with crowding for replacement, binary tournament selection to
+    produce population size children, and a Pareto archival strategy. The remaining operators take
+    on the typical default values but they may be specified by the designer.
+    """
+    _InspyredClass = ec.emo.NSGA2
+
+
+class PAESAlgorithm(MultiObjectiveInspyredAlgorithm):
+    """
+    Evolutionary computation representing the Pareto Archived Evolution Strategy.
+
+    This class represents the Pareto Archived Evolution Strategy of Joshua Knowles and David Corne.
+    It is essentially a (1+1)-ES with an adaptive grid archive that is used as a part of the
+    replacement process.
+    """
+    _InspyredClass = ec.emo.PAES
+    
