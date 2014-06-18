1413f5f5bb047552b17cd6e8e210192abef0702c
Tom Close <tclose@oist.jp>
2014-06-18 17:46:46 +0900

    touched up doc string and default variables

--- neurotune/algorithm/amalgam.py
+++ neurotune/algorithm/amalgam.py
@@ -13,37 +13,14 @@ from amalgam import amalgam_full
 
 class AmalgamAlgorithm(Algorithm):
     """
-    Base class for inspyred evolutionary algorithms based algorithm objects
+    Base class for AMaLGaM IDEA (multiple estimation of distribution) algorithm
+    objects
     """
 
     # Declare this class abstract to avoid accidental construction
     __metaclass__ = ABCMeta
 
     def optimize(self, evaluator, **kwargs):
-        if not self.tuner:
-            raise Exception("optimize method of algorithm must be called from "
-                            "within tuner")
-        self.kwargs.update(kwargs)
-        lbounds, ubounds = zip(*self.constraints)
-        fitness, candidate = amalgam_full(evaluator, self.tuner.num_parameters,
-                                          lbounds, ubounds, **self.kwargs)
-        return candidate, fitness
-
-
-class FullAmalgamAlgorithm(AmalgamAlgorithm):
-    """
-    A python wrapper around the the AMaLGaM-Full estimation of distributions
-    algorithm (IDEA) developed by Peter A.N. Bosmanm, Dirk Thierens and Jorn
-    Grahl
-    """
-
-    _algorithm = amalgam_full
-
-    def __init__(self, rotation_angle, num_populations, max_evaluations,
-                 value_to_reach, fitness_variance_tol, tau=0.35,
-                 population_size=None, distr_mult_decrease=0.9,
-                 st_dev_ratio_threshold=1.0, max_no_improvement=None,
-                 output_dir=None):
         """
         `evaluator`              -- a function that takes a list of parameters
                                     and returns a fitness value
@@ -63,6 +40,53 @@ class FullAmalgamAlgorithm(AmalgamAlgorithm):
                                     solution that is feasible).
         `fitness_variance_tol`   -- The minimum fitness variance level that is
                                     allowed.
+        `tau`                    -- The selection truncation percentile (in
+                                    [1/population_size,1]).
+        `population_size`        -- The size of each population.
+        `distr_mult_decrease`    -- The multiplicative distribution
+                                    multiplier decrease.
+        `st_dev_ratio_threshold` -- The maximum ratio of the distance of the
+                                    average improvement to the mean compared to
+                                    the distance of one standard deviation
+                                    before triggering AVS (SDR mechanism).
+        `max_no_improvement`     -- The maximum number of subsequent
+                                    generations without an improvement while
+                                    the distribution multiplier is <= 1.0.
+        """
+        if not self.tuner:
+            raise Exception("optimize method of algorithm must be called from "
+                            "within tuner")
+        self.kwargs.update(kwargs)
+        lbounds, ubounds = zip(*self.constraints)
+        fitness, candidate = amalgam_full(evaluator, self.tuner.num_parameters,
+                                          lbounds, ubounds, **self.kwargs)
+        return candidate, fitness
+
+
+class FullAmalgamAlgorithm(AmalgamAlgorithm):
+    """
+    A python wrapper around the the AMaLGaM-Full estimation of distributions
+    algorithm (IDEA) developed by Peter A.N. Bosmanm, Dirk Thierens and Jorn
+    Grahl
+    """
+
+    _algorithm = amalgam_full
+
+    def __init__(self, rotation_angle=0.0, num_populations=5,
+                 max_evaluations=1000, fitness_variance_tol=1e-4,
+                 value_to_reach=0.0, tau=0.35, population_size=None,
+                 distr_mult_decrease=0.9, st_dev_ratio_threshold=1.0,
+                 max_no_improvement=None, output_dir=None):
+        """
+        `rotation_angle`         -- The angle of rotation to be applied to the
+                                    problem.
+        `num_populations`        -- The number of parallel populations that
+                                    initially partition the search space.
+        `max_evaluations`        -- The maximum number of evaluations.
+        `fitness_variance_tol`   -- The minimum fitness variance level that is
+                                    allowed.
+        `value_to_reach`         -- The value-to-reach (function value of best
+                                    solution that is feasible).
         `tau`                    -- The selection truncation percentile (in
                                     [1/population_size,1]).
         `population_size`        -- The size of each population.
